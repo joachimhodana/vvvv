eab0b0716a42eba6ef639d9347884be98edeb4ae
style77 <stylek777@gmail.com>
2026-03-16 19:31:21 +0100

    chore(web): use type import in dialog

--- apps/web/components/ui/dialog.tsx
+++ apps/web/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+import type * as React from "react"
 import { Dialog as DialogPrimitive } from "radix-ui"
 import { X } from "@phosphor-icons/react"
 
