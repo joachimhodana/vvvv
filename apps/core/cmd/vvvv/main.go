bedd793d5e2f69bf1dc518a364e535ff9dd63a1b
style77 <stylek777@gmail.com>
2026-03-16 15:21:32 +0100

    feat(core): add main entrypoint

---
+++ apps/core/cmd/vvvv/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joachimhodana/vvvv/core/internal/server"
+)
+
+func main() {
+	addr := ":9194"
+	if fromEnv := os.Getenv("VVVV_CORE_ADDR"); fromEnv != "" {
+		addr = fromEnv
+	}
+
+	srv := server.New(addr)
+
+	log.Printf("vvvv core listening on %s", addr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server error: %v", err)
+	}
+}
+
