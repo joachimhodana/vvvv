4faa19fedf776bf683152f246a7e172a50a37619
style77 <stylek777@gmail.com>
2026-03-17 07:59:38 +0100

    chore(core): remove cmd windows privilege helper

--- apps/core/cmd/vvvv/require_capture_privileges_windows.go
+++
@@ -1,8 +0,0 @@
-//go:build cgo && !nocapture && windows
-
-package main
-
-// Windows capture privilege checks are environment-dependent (Npcap, service permissions).
-// We rely on runtime capture errors to guide the user.
-func requireCapturePrivileges() {}
-
